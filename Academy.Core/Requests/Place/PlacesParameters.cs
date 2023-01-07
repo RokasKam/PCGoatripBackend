850266cb9a210504df96aa3c658f087aeb86ee82
Rokas Kamblevicius <rokaskamble39@gmail.com>
2023-01-07 13:49:17 +0200

    Add PlacesParameters

---
+++ Academy.Core/Requests/Place/PlacesParameters.cs
@@ -0,0 +1,23 @@
+using Academy.Domain.Entities;
+
+namespace Academy.Core.Requests.Place;
+
+public class PlacesParameters
+{
+    const int maxPageSize = 50;
+    
+    public Category FilteringCategory { get; set; }
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 10;
+    public int PageSize
+    {
+        get
+        {
+            return _pageSize;
+        }
+        set
+        {
+            _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+}
